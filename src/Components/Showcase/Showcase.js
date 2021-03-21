402925b6fe2b3a7ea9096c6a9461955fcee25504
Kakie_Wong <kakiewong@gmail.com>
2021-03-21 15:02:13 -0700

    update html/css

--- src/Components/Showcase/Showcase.js
+++ src/Components/Showcase/Showcase.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Showcase.css'
 import hero from '../../FurniturePhotos/chicRoom.jpg'
-import sub1 from '../../FurniturePhotos/bed.jpg'
-import sub2 from '../../FurniturePhotos/decor.jpg'
-import sub3 from '../../FurniturePhotos/diningRoom.jpg'
-import sub4 from '../../FurniturePhotos/bath.jpg'
+import pic1 from '../../FurniturePhotos/bed.jpg'
+import pic2 from '../../FurniturePhotos/decor.jpg'
+import pic3 from '../../FurniturePhotos/diningRoom.jpg'
+import pic4 from '../../FurniturePhotos/bath.jpg'
 import monthly from '../../FurniturePhotos/leatherSofa.jpg'
 import social1 from '../../FurniturePhotos/social1.jpg'
 import social2 from '../../FurniturePhotos/social2.jpg'
@@ -14,30 +14,35 @@ import social4 from '../../FurniturePhotos/social4.jpg'
 function Showcase() {
     return (
         <div className='container'>
+            <div className='services'>
+                <div><p>Free shipping on orders over $249</p></div>
+                <div id='divider'></div>
+                <div><p>Book a free online design consultation</p></div>
+            </div>
             <div className='main'>
                 <div className='hero'><img src={hero} /></div>
                 <div className='sub'>
                     <div className='row1'>
-                        <div className='holder'><img src={sub1} /></div>
-                        <div className='holder'><img src={sub2} /></div>
+                        <div className='pic1'><img src={pic1} /></div>
+                        <div className='pic2'><img src={pic2} /></div>
                     </div>
                     <div className='row2'>
-                        <div className='holder'><img src={sub3} /></div>
-                        <div className='holder'><img src={sub4} /></div>
+                        <div className='pic3'><img src={pic3} /></div>
+                        <div className='pic4'><img src={pic4} /></div>
                     </div>
                 </div>
             </div>
-            <div className='monthly'>
+            <div className='rotatingFeature'>
                 <img src={monthly} />
             </div>
             <div className='social'>
-                <div className='social-holder'><img src={social1} /></div>
-                <div className='social-holder'><img src={social2} /></div>
-                <div className='social-holder'><img src={social3} /></div>
-                <div className='social-holder'><img src={social4} /></div>
-                <div className='social-copy'>
-                    <h4>#FORMSTYLE</h4>
-                    <p>Share your freshest looks with us on Instagram</p>
+                <img src={social1} />
+                <img src={social2} />
+                <img src={social3} />
+                <img src={social4} />
+                <div className='socialText'>
+                    <h3>#FORMSTYLE</h3>
+                    <p>Share your favourite looks with us on social media!</p>
                 </div>
             </div>
         </div>
