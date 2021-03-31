5c8ccd0b81c5df7c1662c18b17a1d4b83fcca8a4
Kakie_Wong <kakiewong@gmail.com>
2021-03-30 21:03:57 -0700

    update img alt

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Card.css'
 
-function Card({ name, price, sku, details, dimensions, img }) {
+function Card({ name, price, img }) {
 
 
     return (
         <div className='card'>
             <h3>{name}</h3>
-            <img className='mainPic' src={img.splash} />
+            <img className='mainPic' src={img.splash} alt={name} />
             <p>${price}</p>
         </div >
 
